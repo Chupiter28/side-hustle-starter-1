d56c73003ea891670e56b509718c79c4f7b0c937
Chupiter28 <chupitersix@gmail.com>
2025-12-01 21:09:36 -0600

    Delete app/app directory

--- app/app/about/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <div className="p-10">
-      <h1 className="text-4xl font-bold">About Us</h1>
-      <p className="mt-4">Learn more about our side hustle!</p>
-    </div>
-  )
-}
